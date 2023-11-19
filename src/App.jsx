ff9c77c67c609e9c6e5ff5bc9f01e2689a52fa45
enzosamojedny <enzosamojedny@gmail.com>
2023-11-19 12:01:12 -0300

    carousel implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
 import ProductDetail from '../src/components/productDetail/ProductDetail';
-
-const productData = {
-  name: 'Producto 1',
-  description: 'Product description...',
+import { ChakraProvider,Flex } from '@chakra-ui/react'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+const productData = [{
+  name: 'Green Sofa',
+  description: 'Immerse yourself in comfort and style with our Green Elegance Sofa. Crafted with precision, this sofa combines modern design with a touch of sophistication. ',
   price: '$29.99',
   sku: 'ABC123',
   quantity: 50,
+  image: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH',
+},
+{
+  name: 'Yellow Sofa',
+  description: 'Immerse yourself in comfort and style with our Yellow Elegance Sofa. Crafted with precision, this sofa combines modern design with a touch of sophistication. ',
+  price: '$49.99',
+  sku: 'ABC1234',
+  quantity: 30,
+  image: 'https://images.unsplash.com/photo-1558211583-d26f610c1eb1?q=80&w=1406&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+},
+{
+  name: 'Black Sofa',
+  description: 'Immerse yourself in comfort and style with our Black Elegance Sofa. Crafted with precision, this sofa combines modern design with a touch of sophistication. ',
+  price: '$80.99',
+  sku: 'ABC1235',
+  quantity: 10,
+  image: 'https://images.unsplash.com/photo-1581699461793-bec4b43073b7?q=80&w=1427&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+}];
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
 };
-
 const App = () => {
   return (
-    <div>
-      <h1>Product Detail</h1>
-      <ProductDetail product={productData} />
-    </div>
+    <ChakraProvider>
+      <Flex direction='column' align='center' justify='center' minH='100vh'>
+        <Slider {...settings}>
+        {productData.map((product, index) => (
+            <ProductDetail key={index} product={product} />
+          ))}
+          </Slider>
+          </Flex>
+    </ChakraProvider>
   );
 };
 
--- src/components/productDetail/ProductDetail.jsx
+++ src/components/productDetail/ProductDetail.jsx
@@ -1,15 +1,58 @@
 import './ProductDetail.css';
-
+import {Card, CardBody,Stack,Heading,Text,Divider,CardFooter,Image,ButtonGroup,Button,useToast, Center} from '@chakra-ui/react'
+/* eslint-disable */
 const ProductDetail = ({ product }) => {
+    const toast = useToast()
     return (
-        <div className="product-detail">
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p className="price">{product.price}</p>
-            <p>SKU: {product.sku}</p>
-            <p className="quantity">{product.quantity}</p>
-            <button className="button">Add to Cart</button>
-        </div>
+  <Card maxW='sm' boxShadow='md' borderRadius='md'>
+      <CardBody>
+      <Center>
+        <Image
+          src={product.image}
+          alt='Green double couch with wooden legs'
+          borderRadius='md'
+        />
+        </Center>
+        <Stack mt='6' spacing='3'>
+          <Heading size='md' fontWeight='bold'>
+            {product.name}
+          </Heading>
+          <Text color='gray.600'>{product.description}</Text>
+          <Text color='blue.600' fontSize='lg' fontWeight='bold'>
+            Price: {product.price}
+          </Text>
+          <Text color='gray.700'>SKU: {product.sku}</Text>
+          <Text color='gray.700'>Available Quantity: {product.quantity}</Text>
+        </Stack>
+      </CardBody>
+      <Divider borderColor='gray.300' />
+      <CardFooter>
+        <ButtonGroup spacing='2'>
+          <Button variant='solid' colorScheme='blue' onClick={() =>
+          toast({
+            title: `You have bought 1 ${product.name}`,
+            description: `For ${product.price}`,
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        }>
+            Buy now
+          </Button>
+          <Button variant='ghost' colorScheme='blue'  onClick={() =>
+          toast({
+            title: ` 1 ${product.name} added to cart`,
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        }>
+            Add to cart
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+
     );
 };
 
