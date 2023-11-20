46030ab106e96a50ada1359540e9835c63908f4c
enzosamojedny <enzosamojedny@gmail.com>
2023-11-20 20:41:23 -0300

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,6 @@ const productData = [{
   sku: 'ABC123',
   quantity: 50,
   image: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH',
-},
-{
-  name: 'Yellow Sofa',
-  description: 'Immerse yourself in comfort and style with our Yellow Elegance Sofa. Crafted with precision, this sofa combines modern design with a touch of sophistication. ',
-  price: '$49.99',
-  sku: 'ABC1234',
-  quantity: 30,
-  image: 'https://images.unsplash.com/photo-1558211583-d26f610c1eb1?q=80&w=1406&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-},
-{
-  name: 'Black Sofa',
-  description: 'Immerse yourself in comfort and style with our Black Elegance Sofa. Crafted with precision, this sofa combines modern design with a touch of sophistication. ',
-  price: '$80.99',
-  sku: 'ABC1235',
-  quantity: 10,
-  image: 'https://images.unsplash.com/photo-1581699461793-bec4b43073b7?q=80&w=1427&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 }];
 const settings = {
   dots: true,
